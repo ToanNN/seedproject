7f3629dc73d7c4d8455217bed76dd7af8f8dcf1f
Toan Nguyen <toan.1aus@gmail.com>
2016-07-10 23:03:07 +1000

    Initial setup

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template:'hello wolrd'
+})
+export class AppComponent{}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by BlueDragon on 10/07/2016.
+ */
+
+import { AppComponent } from './app.component';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+bootstrap(AppComponent);
